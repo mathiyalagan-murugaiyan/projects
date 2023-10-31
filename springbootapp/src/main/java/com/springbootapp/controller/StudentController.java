aa43c958324e1d47e54ea8ac0740dde01b49f58d
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-10-31 13:51:54 +0530

    add delete mapping

--- springbootapp/src/main/java/com/springbootapp/controller/StudentController.java
+++ springbootapp/src/main/java/com/springbootapp/controller/StudentController.java
@@ -1,12 +1,13 @@
 package com.springbootapp.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,30 +21,35 @@ public class StudentController {
 	@Autowired
 	StudentRepo studentrepo;
 
-	@GetMapping("/student")
-	public List<Student> getStudents() {
-		
-//		Map<Integer,String> map = new HashMap<>();
-//	    map.put(1, "mathi");
-//		map.put(2, "marudhu");
-		
-		List<Student> list = new ArrayList<Student>();
-		
-		list = studentrepo.findAll();
-		list.stream()
-		.sorted((s1,s2)-> s1.getName().compareTo(s2.getName()))
-		.forEach(System.out::println);
-				
-		return  list;
-		
-	}
+	Student student;
 
 	@PostMapping("/student")
+	public List<Student> getStudents() {
+
+		List<Student> list = new ArrayList<Student>();
+
+		list = studentrepo.findAll();
+		list.stream().sorted((s1, s2) -> s1.getName().compareTo(s2.getName())).forEach(System.out::println);
+
+		return list;
+
+	}
+
+	@PostMapping("/students")
 	public Student addStudent(@RequestBody Student student) {
 
 		Student s = studentrepo.save(student);
 
 		return s;
+
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteStudent(@PathVariable("id") Long id) {
+
+		studentrepo.deleteById(id);
+
+		return ResponseEntity.ok("deleted successfully");
 
 	}
 
